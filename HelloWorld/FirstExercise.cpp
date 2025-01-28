2b02b9fcb77d71d118b1be29b9ef0f8c7f4c9f13
Josephlodico <164060090+Josephlodico@users.noreply.github.com>
2025-01-27 23:32:07 -0500

    -created a class for UserAccount with variables inside. - created a public constructor for the userAccount.

--- HelloWorld/FirstExercise.cpp
+++ HelloWorld/FirstExercise.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 #include <string>
+using namespace std;  //this makes sure that you don't have to write std all the time
+
+
+class UserAccount  //class Declaration
+{
+private:
+    //variables
+    int ID;
+    string userName;
+    string password;
+
+public:
+    //this is the contructor for the userAccount
+    UserAccount(int id, const string& username, const string& pwd)
+        : ID(id), userName(username), password(pwd) {}
+
+};
 
 int main()
 {
